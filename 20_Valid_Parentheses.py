5389aafcddfaf2e2c26b5e50757ec7375ad19cae
Aeman Abdulmuhssen <60732388+WereWolfx01@users.noreply.github.com>
2022-03-14 17:59:07 -0400

    Create 20_Valid_Parentheses.py

---
+++ 20_Valid_Parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        stack = []
+        for c in s:
+            if c in ['(', '{', '[']:
+                stack.append(c)
+                continue
+            elif c == ')' and len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            elif c == '}' and len(stack) != 0 and stack[-1] == '{':
+                stack.pop()
+            elif c == ']' and len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                return False
+                
+        return stack == []
+                
